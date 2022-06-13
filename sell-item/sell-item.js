f0b283419958f2259daca05db13b72a92611c228
mathiaslvs <mathias.valdebenito@beetrack.com>
2022-06-13 19:49:02 -0400

    add calculated discount

--- sell-item/sell-item.js
+++ sell-item/sell-item.js
@@ -1,4 +1,5 @@
 const template = document.createElement('template');
+//Aunque quite h3 del estilo, el estilo de index-sell-item no le afecta.
 template.innerHTML = `
     <style>
         h3 { 
@@ -17,11 +18,12 @@ template.innerHTML = `
     <div class="sell-item">
         <img />
         <div>
-            <h3></h3>
+            <h3><slot name="name" /></h3>
             <div class="info">
                 <p class="text-box"><slot name="specs" /></p>
-                <p><slot name="discount" /></p>
-                <p><slot name="price" /></p>
+                <p>Descuento: <edit-word><span id=discount></span></edit-word>%</p>
+                <p>Precio normal: $<edit-word><span id="price"></edit-word></span></p>
+                <p>Precio con descuento: $<span id="discounted-price"></span></p>
                 <p><slot name="rating" /></p>
             </div>
             <button id="toggle-info">Hide Info</button>
@@ -37,8 +39,11 @@ class SellItem extends HTMLElement {
 
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true))
-        this.shadowRoot.querySelector("h3"). innerText = this.getAttribute('name');
-        this.shadowRoot.querySelector('img').src = this.getAttribute('item')
+        //this.shadowRoot.querySelector("h3"). innerText = this.getAttribute('name');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('item');
+        this.shadowRoot.querySelector("#discount").innerText =  `${this.getAttribute('discount')}`;
+        this.shadowRoot.querySelector("#price").innerText = `${this.getAttribute('price')}`;
+        this.shadowRoot.querySelector("#discounted-price").innerText =  `${parseInt(this.getAttribute('price') * this.getAttribute('discount')/100)}`;
     }
 
     toggleInfo() {
@@ -64,4 +69,56 @@ class SellItem extends HTMLElement {
     }
 }
 
+class EditWord extends HTMLElement {
+    constructor() {
+        super();
+  
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        const form = document.createElement('form');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+  
+        const style = document.createElement('style');
+        style.textContent = 'span { background-color: #eef; padding: 0 2px }';
+  
+        shadowRoot.appendChild(style);
+        shadowRoot.appendChild(form);
+        shadowRoot.appendChild(span);
+  
+        span.textContent = this.textContent;
+        input.value = this.textContent;
+  
+        form.appendChild(input);
+        form.style.display = 'none';
+        span.style.display = 'inline-block';
+        input.style.width = span.clientWidth + 'px';
+  
+        this.setAttribute('tabindex', '0');
+        input.setAttribute('required', 'required');
+        this.style.display = 'inline-block';
+  
+        this.addEventListener('click', () => {
+          span.style.display = 'none';
+          form.style.display = 'inline-block';
+          input.focus();
+          input.setSelectionRange(0, input.value.length)
+        });
+  
+        form.addEventListener('submit', e => {
+          updateDisplay();
+          e.preventDefault();
+        });
+  
+        input.addEventListener('blur', updateDisplay);
+  
+        function updateDisplay() {
+          span.style.display = 'inline-block';
+          form.style.display = 'none';
+          span.textContent = input.value;
+          input.style.width = span.clientWidth + 'px';
+        }
+    }
+}
+
 window.customElements.define('sell-item', SellItem);
+window.customElements.define('edit-word', EditWord);
